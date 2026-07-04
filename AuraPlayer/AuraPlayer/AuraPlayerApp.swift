8436ba8cff5b8b9871c0619484d660af0adb3f72
Thinh Nguyen <thinhduc.ngu@ascendcorp.com>
2026-07-04 19:24:09 +0700

    init: AuraPlayer Xcode project

---
+++ AuraPlayer/AuraPlayer/AuraPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraPlayerApp.swift
+//  AuraPlayer
+//
+//  Created by mobile on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AuraPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraPlayer/AuraPlayerTests/AuraPlayerTests.swift
@@ -0,0 +1,19 @@
+//
+//  AuraPlayerTests.swift
+//  AuraPlayerTests
+//
+//  Created by mobile on 4/7/26.
+//
+
+import Testing
+@testable import AuraPlayer
+
+struct AuraPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
